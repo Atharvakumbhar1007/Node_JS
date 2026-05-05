ccd1210e85c8e2c8cd8f0e3ad03ff8fd0572e154
Atharva <atharvakumbhar631@gmail.com>
2026-05-05 17:15:42 +0530

    basics

---
+++ firstProj/index.js
@@ -0,0 +1,9 @@
+import chalk from "chalk";
+
+console.log(chalk.blue.underline("Hello World!"));
+console.log(chalk.red.bold("Error!"));
+
+//inbuild keywords
+//to run these you can use just write npm stop , npm start etc
+//your customer keywoards will need run
+//ex: npm run bulid, npm run test
